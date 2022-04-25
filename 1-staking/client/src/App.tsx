ab6ee7b27221ed9b6c3d23e8bec24a7297527ffd
gareth-was-here <gareth@garethveale.dev>
2022-04-25 17:22:43 +0100

    restyle + install ethersJs

--- 1-staking/client/src/App.tsx
+++ 1-staking/client/src/App.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ethers } from "ethers";
 import './App.css';
 
 function App() {
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('get metamask!');
+        return
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Connected!", accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }  
+
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">Staking</p>
+          <p className="sub-text">Stake your eth</p>
+          { currentAccount === "" ? ( 
+              renderNotConnectedContainer() 
+            ) : (  
+              <button className="cta-button connect-wallet-button">
+                Stake Eth
+              </button>
+            ) 
+          }
+        </div>
+        <div className="footer-container"></div>
+      </div>
     </div>
   );
 }
